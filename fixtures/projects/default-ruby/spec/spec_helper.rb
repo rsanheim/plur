a3a707fcbd5aec4cf8a52ff20be3515340e59ab8
Rob Sanheim <rsanheim@gmail.com>
2025-07-14 14:29:18 -0500

    Fix syntax error in fixture spec_helper.rb
    The require statement had invalid syntax with a misplaced asterisk and quote.
    Changed from: require.*plur_ruby"
    To: require "plur_ruby"

--- fixtures/projects/default-ruby/spec/spec_helper.rb
+++ fixtures/projects/default-ruby/spec/spec_helper.rb
@@ -1,7 +1,7 @@
 base_path = File.expand_path("../../lib", __dir__)
 $LOAD_PATH.unshift(base_path)
 
-require.*plur_ruby"
+require "plur_ruby"
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
