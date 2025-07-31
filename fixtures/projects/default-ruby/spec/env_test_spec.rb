b90916c449cc757c322a599ac37695b8fc25e146
Rob Sanheim <rsanheim@gmail.com>
2025-07-31 04:26:13 -0500

    ooof i forgot we reset fixtures

--- fixtures/projects/default-ruby/spec/env_test_spec.rb
+++ fixtures/projects/default-ruby/spec/env_test_spec.rb
@@ -1,9 +1,11 @@
 require "spec_helper"
 
+# rubocop:disable RuboCop::Cop::Style::StderrPuts 
 RSpec.describe "Environment Variables" do
   it "prints TEST_ENV_NUMBER and PARALLEL_TEST_GROUPS" do
-    puts "TEST_ENV_NUMBER: '#{ENV["TEST_ENV_NUMBER"]}'"
-    puts "PARALLEL_TEST_GROUPS: '#{ENV["PARALLEL_TEST_GROUPS"]}'"
+    warn "TEST_ENV_NUMBER: '#{ENV["TEST_ENV_NUMBER"]}'"
+    warn "PARALLEL_TEST_GROUPS: '#{ENV["PARALLEL_TEST_GROUPS"]}'"
     expect(true).to be true
   end
 end
+# rubocop:enable RuboCop::Cop::Style::StderrPuts 
